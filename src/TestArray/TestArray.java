5aa7fd0fda4692219f4f747dee9fedee1dbb91ab
ZX <2327theforce@gmail.com>
2018-10-12 17:20:36 +0800

    ok

--- src/TestArray/TestArray.java
+++ src/TestArray/TestArray.java
@@ -5,17 +5,11 @@ public class TestArray
     public static void main(String[] args)
     {
         int[] sim = {2, 4, 5, 9, 234, 6, 23};
+
+        //初始化
         ArraySort array = new ArraySort(sim);
+
         array.setOrder();
         array.getArray();
-//        //Arrays.toString(sim);
-//        Arrays.sort(sim);
-//        for (int e:sim)
-//        {
-//            System.out.println(e);
-//        }
-//
-//        //int[] copy = Arrays.copyOf(Arrays.sort(sim), sim.length);
-//
     }
 }
